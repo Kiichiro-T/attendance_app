dd4dd1194b2c5d840dbe96fb3651980fe5799bb3
EC2 Default User <ec2-user@ip-172-31-34-89.ap-northeast-1.compute.internal>
2019-08-16 06:57:17 +0000

    タイトル部分の微調整

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -14,19 +14,24 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get help" do
     get help_path
     assert_response :success
-    assert_select "title", "Help | #{@basetitle}"
+    assert_select "title", "ヘルプ | #{@basetitle}"
   end
 
   test "should get about" do
     get about_path
     assert_response :success
-    assert_select "title", "About | #{@basetitle}"
+    assert_select "title", "このサイトについて | #{@basetitle}"
   end
   
   test "should get contact" do
     get contact_path
     assert_response :success
-    assert_select "title", "Contact | #{@basetitle}"
+    assert_select "title", "お問い合わせ | #{@basetitle}"
   end
   
+  test "should get signup" do
+    get signup_path
+    assert_response :success
+    assert_select "title", "サインアップ | #{@basetitle}"
+  end
 end
