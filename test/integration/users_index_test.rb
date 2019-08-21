58c9f90753ded6c2f8e5e602584f622dd06a7d20
EC2 Default User <ec2-user@ip-172-31-34-89.ap-northeast-1.compute.internal>
2019-08-21 03:36:23 +0000

    Add account activation

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -5,14 +5,14 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
   def setup
     @admin      = users(:michael)
     @non_admin = users(:archer)
+    @non_activated = users(:bob)
   end
   
   test "index as admin including pagination and delete links" do
     log_in_as(@admin)
     get users_path
     assert_template 'users/index'
-    assert_select "div.pagination", count: 2
-    first_page_of_users = User.paginate(page: 1)
+    first_page_of_users = User.where(activated: true).paginate(page: 1)
     first_page_of_users.each do |user|
       assert_select "a[href=?]", user_path(user), text: user.name
       unless user == @admin
