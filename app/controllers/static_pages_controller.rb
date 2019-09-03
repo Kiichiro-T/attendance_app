05bcb926f6ee5b2bf09a788987213f008b75b394
EC2 Default User <ec2-user@ip-172-31-34-89.ap-northeast-1.compute.internal>
2019-09-03 13:08:51 +0000

    Finish refuctoring

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,9 +2,11 @@ class StaticPagesController < ApplicationController
    
   def home
     scope = Event.joins(:answers)
-    id = current_user.id
-    @not_answered_event = current_user.events.left_joins(:answers).where(answers: { id: nil }).paginate(page: params[:page]) if logged_in?
-    @answered_events = scope.where("answers.user_id = ?", id).paginate(page: params[:page]) if logged_in?
+    if current_user
+      id = current_user.id
+      @not_answered_event = current_user.events.left_joins(:answers).where(answers: { id: nil }).paginate(page: params[:page]) if logged_in?
+      @answered_events = scope.where("answers.user_id = ?", id).paginate(page: params[:page]) if logged_in?
+    end
   end
 
   def help
